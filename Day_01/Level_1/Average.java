94f15fc8c58a3ad5d254de1bb5ea889abbd10ad5
Vishal Jadhav <vishaljadhav56561@gmail.com>
2025-01-11 12:41:12 +0530

    Deleted level_1

--- Day_01/Level_1/Average.java
+++
@@ -1,9 +0,0 @@
-public class Average
-{
-public static void main(String []args)
-{
-int math=94, physics=95,chemistry=96;
-double Average=(94+95+96)/4;
-System.out.println("Sam;s average mark in PCM is " + Average);
-}
-}
--- Day_01/Level_1/Convert.java
+++
@@ -1,9 +0,0 @@
-public class Convert
-{
-public static void main(String args[])
-{
-double kilometer=10.8;
-double miles=kilometer*0.6213711922;
-System.out.println("The distanec " + kilometer + " km in miles is " + miles);
-}
-}
